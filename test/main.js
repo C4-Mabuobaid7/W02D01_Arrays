9ad11dae102b6fdba49738120501a65e8464b968
mabuobaid7 <93932080+mabuobaid7@users.noreply.github.com>
2021-11-18 17:08:26 +0200

    Add files via upload

---
+++ test/main.js
@@ -0,0 +1,38 @@
+//q1
+
+
+const colors = [red , green , pink];
+
+
+const negativenumbers= [1 , 2 ,3 ,4];
+
+
+const food=[mansaf , kabsa , shwarma];
+
+
+const numbers=[[1,3,5] , [2,4,6,]];
+
+//q2
+
+const orderedPlanets=[3];
+const unorderedPlanets =[0];
+
+const animals = [animals . linght-1];
+
+
+//q3
+orderplanet . push (mars);
+
+unorderedPlanets 
+unorderedPlanets[unorderedPlanets . lingth] ="mars";
+
+
+animals[0]="koala";
+
+
+//q4
+
+const reptiles =  reptiles .push(dinosaur);
+
+const fish = fish.pop("coldfish");
+
